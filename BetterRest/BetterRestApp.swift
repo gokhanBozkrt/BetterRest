4ef600c2c1accb5629178f136b5bde19d35fe9e6
gokhanBozkrt <99559446+gokhanBozkrt@users.noreply.github.com>
2022-04-22 18:41:23 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Gokhan Bozkurt on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
